c6ad2b5c8c412cc408d3ef848330f580672a373f
Sakshi Jadaun <sakshijadaun080@gmail.com>
2023-05-08 16:04:02 +0530

    Add Prop to CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
+  
     // this.increaseQuantity = this.increaseQuantity.bind(this);
-  }
+  
   increaseQuantity = () => {
     //console.log('this', this.state);
     //setState form 1
@@ -39,7 +32,8 @@ class CartItem extends React.Component {
     });
   }
   render () {
-    const { price, title, qty } = this.state;
+    console.log('this.props',this.props);
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
